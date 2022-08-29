68bd2038de3adf6f5d410688b542a334c7272607
Daniel Kraemer <dkraemer@dkross.org>
2022-08-30 01:40:08 +0200

    Initial commit

---
+++ Assets/Editor/Screenshot.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace DKrOSS.Unity.Editor
+{
+    public class Screenshot
+    {
+        [MenuItem("DKrOSS/Take Screenshot")]
+        public static void Grab()
+        {
+            var timestamp = DateTime.Now.ToString("yyyyMMdd-HHmmss");
+            var basePath = Path.Combine(".", "Assets", "Screenshots");
+            var fileName = $"Screenshot_{timestamp}_{Screen.width}x{Screen.height}.png";
+            var path = Path.Combine(basePath, fileName);
+            
+            if(!Directory.Exists(basePath))
+            {
+                Directory.CreateDirectory(basePath);
+            }
+
+            ScreenCapture.CaptureScreenshot(path);
+        }
+    }
+}
+#endif
